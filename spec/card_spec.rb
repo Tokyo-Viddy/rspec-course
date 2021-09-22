3f1f18bb7d9d36ebdef8aeb61271f35614371d70
David Gregorato <HydroSnake@gmail.com>
2021-09-22 10:45:22 +0900

    Made a card class to pass the card test.

---
+++ spec/card_spec.rb
@@ -0,0 +1,15 @@
+# Start of the card test file
+class Card
+  attr_reader :type
+
+  def initialize(type)
+    @type = type
+  end
+end
+
+RSpec.describe 'Card' do
+  it 'has a type' do # specify 'has a type' do
+    card = Card.new('Ace of Spades')
+    expect(card.type).to eq('Ace of Spades')
+  end
+end
