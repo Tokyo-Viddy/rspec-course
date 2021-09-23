cc9ba92e88451e657caa0cf933aaa82e48112b8a
David Gregorato <HydroSnake@gmail.com>
2021-09-23 23:17:16 +0900

    Changed rspec file to put let isntead of a method.

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,15 +1,29 @@
 # Start of the card test file
 class Card
-  attr_reader :type
+  attr_reader :suit
+  attr_accessor :rank
 
-  def initialize(type)
-    @type = type
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
   end
 end
 
-RSpec.describe 'Card' do
-  it 'has a type' do # specify 'has a type' do
-    card = Card.new('Ace of Spades')
-    expect(card.type).to eq('Ace of Spades')
+RSpec.describe Card do
+  let(:card) { Card.new('Ace', 'Spades') }
+
+  it 'has a rank and that rank can change' do
+    expect(card.rank).to eq('Ace')
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
+  end
+
+  it 'has a suit' do
+    expect(card.suit).to eq('Spades')
+  end
+
+  it 'has a custon error message' do
+    comparison = 'Spades'
+    expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} but I got #{card.suit} instead!"
   end
 end
