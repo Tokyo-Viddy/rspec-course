f6b8ffcd3cd6e568de13bd7309f8889c88b48b72
David Gregorato <HydroSnake@gmail.com>
2021-09-24 13:12:32 +0900

    Made nexted hooks spec file.

---
+++ spec/nested_hooks_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe 'nested hooks' do
+  before(:context) do
+    puts 'OUTER Before context'
+  end
+
+  before(:example) do
+    puts 'OUTER Before example'
+  end
+
+  it 'does basic math' do
+    expect(3 * 3).to eq(9)
+  end
+
+  context 'with condition A' do
+    before(:context) do
+      puts 'INNER Before context'
+    end
+
+    before(:example) do
+      puts 'INNER Before example'
+    end
+
+    it 'does more math' do
+      expect(12 / 4).to eq(3)
+    end
+
+    it 'also does this sort of math' do
+      expect(7 - 2).to eq(5)
+    end
+  end
+end
