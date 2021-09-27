4217806a34f21bacaf9d01528f9d3517e2474ada
David Gregorato <HydroSnake@gmail.com>
2021-09-27 18:46:31 +0900

    Finished one-liner syntax exercise.

--- spec/one_liner_syntax_spec.rb
+++ spec/one_liner_syntax_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'shorthand syntax' do
   end
 
   context 'with one-liner syntax' do
-    it { is_expected.to eq(5) }
+    it { is_expected.to eq(5) } # Same as lines 5-6
   end
 
 end
