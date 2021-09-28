20f3fb6e4c9a74fdc21c894d3425afeebb9f7412
David Gregorato <HydroSnake@gmail.com>
2021-09-28 14:31:36 +0900

    Finished the predicate methods exercises.

---
+++ spec/predicate_methods_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe 'predicate methods and predicate matchers' do
+  it 'can be tested with Ruby methods' do
+    result = 16 / 2
+    expect(result.even?).to eq(true)
+  end
+
+  it 'can be tested with predicate matchers' do
+    expect(16 / 2).to be_even
+    expect(15).to be_odd
+    expect([]).to be_empty
+  end
+end
